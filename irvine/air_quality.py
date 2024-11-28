967d593c86bf9cf06374edddda1e1f4076b56241
John Hanley <jhanley708-git@s6.sector6.net>
2024-11-27 17:01:34 -0800

    Add _df_neg_is_nan

--- irvine/air_quality.py
+++ irvine/air_quality.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 from ucimlrepo import fetch_ucirepo
@@ -20,9 +21,15 @@ def main() -> None:
     assert air_quality.data.targets is None
 
     x["stamp"] = pd.to_datetime(x["Date"] + " " + x["Time"], format="%m/%d/%Y %H:%M:%S")
-    x["Time"] = pd.to_timedelta(x["Time"])
+    x["Time"] = pd.to_timedelta(x["Time"]).dt.total_seconds()
     x = x.drop(columns=["Date"])
-    x = _extract_reference_features(x).drop(columns=['nmhc',"abs_humid"])
+    x = _extract_reference_features(x)
+    x = x.drop(columns=["nmhc", "abs_humid"])
+    x = _df_neg_is_nan(x)
+
+    for col in x.columns:
+        if col != "stamp":
+            x[col] = x[col].astype(float)
 
     x.info()
     print(x.describe())
@@ -54,6 +61,19 @@ def _extract_reference_features(x: pd.DataFrame) -> pd.DataFrame:
     }
     x = x.rename(columns=new_names)
     return x[list(new_names.values())]
+
+
+def _series_neg_is_nan(x: "pd.Series[float]", sentinel: int = -200) -> "pd.Series[float]":
+    """In this dataset, it turns out -200 is used to represent missing values."""
+
+    def to_nan(v: float) -> float:
+        return v if v != sentinel else np.nan
+
+    return x.apply(to_nan)
+
+
+def _df_neg_is_nan(x: pd.DataFrame) -> pd.DataFrame:
+    return x.apply(_series_neg_is_nan)
 
 
 if __name__ == "__main__":
