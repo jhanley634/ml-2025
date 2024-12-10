722750cd4d88878b2570f21c72b983ebcf403939
John Hanley <jhanley634@users.noreply.github.com>
2024-12-09 21:59:33 -0800

    6 holdout (#7)
    * Delete unused ydata-profile 3.12 venv, as it turns out the main project venv cannot use 3.13 anyway
    
    * Add ModelType TypeVar
    
    * Delete xgboost
    
    * Add holdout_split
    
    * Add report()
    
    * Add create_models()
    
    * Add @beartype
    
    * Consistently pass around NDArray[np.float64] arrays
    
    * Elide ForestRegressor
    
    * Increase the learning rate
    
    * Simplify conversion to torch Tensor
    
    * Add ElasticNet
    
    * Add SVR-Linear
    
    * Add search_for_svr_hyperparams
    
    * Add load_or_search_for_svr_hyperparams
    
    * Add juypter
    
    * Use the .json filename spelling

--- Dockerfile
+++ Dockerfile
@@ -37,7 +37,7 @@ USER ml
 RUN (curl -LsSf https://astral.sh/uv/install.sh | sh) && \
     . $HOME/.bashrc && \
     . $HOME/.local/bin/env && \
-    cp -p profile/.gitconfig $HOME && \
+    cp -p irvine/.gitconfig $HOME && \
     make install
 
 ENTRYPOINT ["bash"]
--- irvine/air_quality_model.py
+++ irvine/air_quality_model.py
@@ -1,22 +1,29 @@
 #! /usr/bin/env python
 from collections.abc import Iterable
+from pathlib import Path
+from typing import TypeVar
 
 import numpy as np
 import pandas as pd
 import torch
+import yaml
+from beartype import beartype
 from numpy.typing import NDArray
+from scipy.stats import loguniform, uniform
 from sklearn.ensemble import HistGradientBoostingRegressor, RandomForestRegressor
-from sklearn.ensemble._forest import ForestRegressor
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import ElasticNet, LinearRegression
 from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from sklearn.neighbors import KNeighborsRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
-from torch import Tensor, float32, nn, optim, tensor
+from torch import Tensor, nn, optim
+from tqdm import tqdm
 
 from irvine.air_quality_eda import get_air_quality_dataset
 
 
+@beartype
 class LSTM(nn.Module):
     def __init__(self, input_size: int, hidden_layer_size: int = 50) -> None:
         super().__init__()
@@ -30,49 +37,88 @@ class LSTM(nn.Module):
         return ret
 
 
+ModelType = TypeVar(
+    "ModelType",
+    ElasticNet,
+    HistGradientBoostingRegressor,
+    KNeighborsRegressor,
+    LSTM,
+    LinearRegression,
+    RandomForestRegressor,
+    RandomizedSearchCV,
+    SVR,
+)
+
+
+@beartype
 def train_evaluate_sklearn_model(
-    model: ForestRegressor | HistGradientBoostingRegressor | LinearRegression | SVR,
-    x_train: Tensor,
-    y_train: Iterable[float],
+    model: ModelType,
+    x_train: NDArray[np.float64],
+    y_train: NDArray[np.float64],
     x_test: NDArray[np.float64],
     y_test: Iterable[float],
 ) -> dict[str, float]:
+    if isinstance(model, LSTM):
+        return train_evaluate_lstm_model(model, x_train, y_train, x_test, y_test)
+
     model.fit(x_train, y_train)
     y_pred = model.predict(x_test)
+
+    score = model.score(x_test, y_test)
+    assert isinstance(score, float)
     return {
         "rmse": float(mean_squared_error(y_test, y_pred)),
-        "r2": float(model.score(x_test, y_test)),
+        "r2": score,
     }
 
 
-def train_evaluate_lstm(
-    x_train: Tensor,
+@beartype
+def train_evaluate_lstm_model(
+    model: LSTM,
+    x_train: NDArray[np.float64],
     y_train: NDArray[np.float64],
-    x_test: Tensor,
+    x_test: NDArray[np.float64],
     y_test: Iterable[float],
-    epochs: int = 10,
 ) -> dict[str, float]:
-    model = LSTM(input_size=x_train.shape[2])
+    epochs: int = 200
     criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=0.04)
+    x_train_tensor = Tensor(x_train).unsqueeze(1)
+    x_test_tensor = Tensor(x_test).unsqueeze(1)
     y_train_tensor = Tensor(y_train).unsqueeze(-1)  # Adds a dimension to make it [7192, 1]
 
-    for _epoch in range(epochs):
+    for name, param in model.lstm.named_parameters():
+        if "weight" in name:
+            torch.nn.init.xavier_uniform_(param)
+        elif "bias" in name:
+            torch.nn.init.zeros_(param)
+
+    for _ in tqdm(range(epochs), leave=False):
         model.train()
         optimizer.zero_grad()
-        y_pred = model(x_train)
+        y_pred = model(x_train_tensor)
         loss = criterion(y_pred, y_train_tensor)
         loss.backward()
         optimizer.step()
 
     model.eval()
     with torch.no_grad():
-        y_pred = model(x_test)
-        return {"rmse": mean_squared_error(y_test, y_pred), "r2": 0.0}
+        y_pred = model(x_test_tensor)
+        return {
+            "rmse": float(mean_squared_error(y_test, y_pred)),
+            "r2": 0.0,
+        }
 
 
+@beartype
 def main() -> None:
     df = get_air_quality_dataset().dropna(subset=["benzene"])
+    holdout_split = 1800
+    holdout = df.tail(holdout_split)
+    df = df.head(len(df) - holdout_split)
+    assert len(holdout) == holdout_split
+    assert len(df) == 7191
+
     y = df["benzene"]
     x = df.drop(columns=["benzene"])
 
@@ -83,34 +129,92 @@ def main() -> None:
     x_train_scaled = scaler.fit_transform(x_train)
     x_test_scaled = scaler.transform(x_test)
     assert isinstance(x_test_scaled, np.ndarray)
+    x_test_scl = x_test_scaled.astype(np.float64)
 
+    create_models(x_train_scaled, y_train.to_numpy(), x_test_scl, pd.Series(y_test).to_numpy())
+
+
+@beartype
+def create_models(
+    x_train: NDArray[np.float64],
+    y_train: NDArray[np.float64],
+    x_test: NDArray[np.float64],
+    y_test: NDArray[np.float64],
+) -> None:
     models = {
-        "HistGradientBoosting": HistGradientBoostingRegressor(),
-        "RandomForest": RandomForestRegressor(),
-        "SVR": SVR(),
+        "ElasticNet": ElasticNet(),
+        "HistGradientBoostingRegressor": HistGradientBoostingRegressor(),
+        "K-Nearest Neighbors": KNeighborsRegressor(),
+        "LSTM": LSTM(input_size=x_train.shape[1]),
         "LinearRegression": LinearRegression(),
+        "RandomForestRegressor": RandomForestRegressor(),
+        "SVR-RBF": load_or_search_for_svr_hyperparams(
+            x_train,
+            y_train,
+        ),  # This uses the caching mechanism
     }
 
     results = {}
     for name, model in models.items():
+        print(name)
         results[name] = train_evaluate_sklearn_model(
             model,
-            x_train_scaled,
+            x_train,
             y_train,
-            x_test_scaled.astype(np.float64),
+            x_test,
             y_test,
         )
 
-    x_train_lstm = tensor(x_train_scaled, dtype=float32).unsqueeze(1)
-    x_test_lstm = tensor(x_test_scaled, dtype=float32).unsqueeze(1)
+    report(results)
 
-    results["LSTM"] = train_evaluate_lstm(x_train_lstm, y_train.to_numpy(), x_test_lstm, y_test)
 
+PARAM_CACHE = Path("/tmp/svr_params.json")
+
+
+@beartype
+def load_or_search_for_svr_hyperparams(
+    x_train: NDArray[np.float64],
+    y_train: NDArray[np.float64],
+) -> RandomizedSearchCV:
+    if PARAM_CACHE.exists():
+        with PARAM_CACHE.open() as fin:
+            return yaml.load(fin, Loader=yaml.FullLoader)
+
+    svr_search = search_for_svr_hyperparams()
+    svr_search.fit(x_train, y_train)
+
+    with PARAM_CACHE.open("w") as fout:
+        yaml.dump(svr_search.best_params_, fout)
+
+    return svr_search
+
+
+@beartype
+def search_for_svr_hyperparams() -> RandomizedSearchCV:
+    svr_param_grid = {
+        "C": loguniform(1e-2, 1e3),
+        "epsilon": uniform(0.01, 0.5),
+        "kernel": ["linear", "rbf"],
+        "gamma": ["scale", "auto"],
+    }
+    return RandomizedSearchCV(
+        SVR(kernel="rbf"),
+        svr_param_grid,
+        n_iter=10,
+        cv=5,
+        verbose=1,
+        random_state=42,
+        n_jobs=-1,
+    )
+
+
+@beartype
+def report(results: dict[str, dict[str, float]]) -> None:
     for name, metrics in results.items():
         print(f"Model: {name}")
-        print(f"RMSE: {metrics['rmse']}")
+        print(f"RMSE: {metrics['rmse']:.4f}")
         if metrics["r2"] is not None:
-            print(f"R^2: {metrics['r2']}")
+            print(f"R^2:  {metrics['r2']:.4f}")
         print("-" * 40)
 
 
