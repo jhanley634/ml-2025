5807981117c5ca583d123cf21f76661a541cfee5
John Hanley <jhanley634@users.noreply.github.com>
2025-03-21 17:14:31 -0700

    Use multiple models

--- llm/prompt/stability.py
+++ llm/prompt/stability.py
@@ -1,10 +1,28 @@
 #! /usr/bin/env streamlit run --server.runOnSave true --server.headless true --browser.gatherUsageStats false
 
+from dataclasses import dataclass
+
 import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama import OllamaLLM
 
 from llm.prompt.config import CHAT_PROMPT_TEMPLATE
+
+
+@dataclass
+class LLM:
+    name: str
+    model_instance: OllamaLLM
+
+
+def _get_model(name: str) -> LLM:
+    return LLM(name, OllamaLLM(model=name))
+
+
+models = [
+    _get_model("gemma"),
+    _get_model("phi4"),
+]
 
 
 def _msg(role: str, content: str) -> dict[str, str]:
@@ -14,16 +32,11 @@ def _msg(role: str, content: str) -> dict[str, str]:
     }
 
 
-model = OllamaLLM(model="phi4")
-
-
-def response_from_single_model() -> None:
+def response_from_multiple_models() -> None:
     st.markdown(
-        "<h2 style='text-align: center; font-family: Arial;'>LLM</h2>",
+        "<h2 style='text-align: center; font-family: Arial;'>LLMs</h2>",
         unsafe_allow_html=True,
     )
-    prompt = ChatPromptTemplate.from_template(CHAT_PROMPT_TEMPLATE)
-    chain = prompt | model
 
     if "messages" not in st.session_state:
         empty: list[dict[str, str]] = []
@@ -40,13 +53,17 @@ def response_from_single_model() -> None:
         with st.chat_message("user"):
             st.markdown(user_input)
 
+    prompt = ChatPromptTemplate.from_template(CHAT_PROMPT_TEMPLATE)
     with st.chat_message("assistant"):
-        response = chain.invoke({"question": user_input})
-        st.markdown(response)
-        st.session_state.messages.append(
-            _msg("assistant", response),
-        )
+        for model in models:
+            chain = prompt | model.model_instance
+            response = chain.invoke({"question": user_input})
+            st.markdown(f"### {model.name}:")
+            st.markdown(response)
+            st.session_state.messages.append(
+                _msg("assistant", response),
+            )
 
 
 if __name__ == "__main__":
-    response_from_single_model()
+    response_from_multiple_models()
