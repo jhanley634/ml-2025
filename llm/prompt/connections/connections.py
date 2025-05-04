511eeb1f30d8f06977ac03939a7e45f87d4ed66b
John Hanley <jhanley634@users.noreply.github.com>
2025-05-04 13:58:40 -0700

    Expose "canonicalize the markdown" as a public function

--- llm/prompt/connections/connections.py
+++ llm/prompt/connections/connections.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python
 
+import re
+
 from langchain_core.messages import AIMessage
 from langchain_ollama import ChatOllama
 
 from llm.prompt.connections.example_instances import examples, prompt
-from llm.prompt.connections.util import as_df
+from llm.prompt.connections.util import as_df, canonicalize
 
 
 def get_llm_response(prompt: str, model: str = "phi4") -> str:
@@ -18,8 +20,10 @@ def get_llm_response(prompt: str, model: str = "phi4") -> str:
 def main() -> None:
     for squished, result in reversed(examples):
         df = as_df(result)
-        print(df.to_markdown(index=False).replace(":", "-"))
-        print(get_llm_response(f"{prompt}\n\n{squished}"))
+        md_tbl = df.to_markdown(index=False).replace(":", "-")
+        print(canonicalize(re.sub(r" +", " ", md_tbl)))
+        response = get_llm_response(f"{prompt}\n\n{squished}")
+        print(canonicalize(response))
 
 
 if __name__ == "__main__":
--- llm/prompt/connections/util.py
+++ llm/prompt/connections/util.py
@@ -7,16 +7,17 @@ temp = Path("/tmp") / "connections_result.csv"
 
 
 def as_df(markdown_table: str) -> pd.DataFrame:
-    text = "CATEGORY|WORDS\n" + _strip_leading_and_trailing_pipes(markdown_table.lstrip())
+    text = "CATEGORY|WORDS\n" + canonicalize(markdown_table.lstrip())
     temp.write_text(text)
     return pd.read_csv(temp, sep="|", engine="python")
 
 
-def _strip_leading_and_trailing_pipes(markdown_table: str) -> str:
+def canonicalize(markdown_table: str) -> str:
     r"""Makes a markdown formatted table look more like a CSV.
 
-    >>> _strip_leading_and_trailing_pipes("| a b | c d e f |\n|---|---|")
-    'a b|c d e f\n---|---'
+    >>> canonicalize("| a b   | c d e f   |\n|-----|-------|")
+    'a b | c d e f\n----|----'
     """
-    tbl = re.sub(r" *\| *", "|", markdown_table, flags=re.MULTILINE)  # 'x | y' --> 'x|y'
-    return re.sub(r"^\||\|$", "", tbl, flags=re.MULTILINE)
+    tbl = re.sub(r" +", " ", markdown_table, flags=re.MULTILINE)  # no repeated blanks
+    tbl = re.sub(r"----+", "----", tbl, flags=re.MULTILINE)  # always 4 dashes
+    return re.sub(r"^\| *| *\|$", "", tbl, flags=re.MULTILINE)  # '|x|y|' --> 'x|y'
