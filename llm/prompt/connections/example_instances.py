6111ba0b5f8dc625cb35de62c1844af3f23d242c
John Hanley <jhanley634@users.noreply.github.com>
2025-05-04 10:10:14 -0700

    Break out connections/example_instances.py

---
+++ llm/prompt/connections/example_instances.py
@@ -0,0 +1,94 @@
+#! /usr/bin/env python
+
+
+prompt = """
+Produce a two-column four-row markdown table using |- + characters,
+which maps from CATEGORY to four comma-separated WORDS or phrases within the category.
+Be sure to place a ", " COMMA SPACE between each of those four WORDS.
+Everything in both columns shall be in ALL CAPS.
+"""
+examples = [
+    (
+        """
+Create four groups of four!
+
+4 FoundCategories out of 4
+
+    SEEN IN A POTTERY STUDIO
+        CLAYGLAZEKILNWHEEL
+    THINGS THAT ARE SLIPPERY
+        BANANA PEELEELGREASEICE
+    NATURAL PRODUCERS OF HEAT
+        FIRELIGHTNINGSUNVOLCANO
+    CANCEL, AS A PROJECT
+        AXECUTDROPSCRAP
+""",
+        """
+| SEEN IN A POTTERY STUDIO          | CLAY, GLAZE, KILN, WHEEL                       |
+| THINGS THAT ARE SLIPPERY          | BANANA PEEL, EEL, GREASE, ICE                  |
+| NATURAL PRODUCERS OF HEAT         | FIRE, LIGHTNING, SUN, VOLCANO                  |
+| CANCEL, AS A PROJECT              | AXE, CUT, DROP, SCRAP                          |
+""",
+    ),
+    (
+        """
+Create four groups of four!
+
+4 FoundCategories out of 4
+
+    KINDS OF CARVINGS
+        BUSTRELIEFSTATUETORSO
+    PILLAR
+        BRACEPOSTPROPSUPPORT
+    BBQ OFFERING
+        DOGLINKRIBWING
+    ___NECK
+        BOTTLEBREAKGOOSETURTLE
+""",
+        """
+| KINDS OF CARVINGS                 | BUST, RELIEF, STATUE, TORSO                    |
+| PILLAR                            | BRACE, POST, PROPS, SUPPORT                    |
+| BBQ OFFERING                      | DOG, LINK, RIB, WING                           |
+| ___NECK                           | BOTTLE, BREAK, GOOSE, TURTLE                   |
+""",
+    ),
+    (
+        """
+ Create four groups of four!
+
+4 FoundCategories out of 4
+
+    AWESOME
+        DOPEFIRELITSICK
+    URL ENDINGS PLUS A LETTER
+        COMPMILKNETIORGO
+    DEFEAT SOUNDLY
+        CREAMLICKPASTESMOKE
+    “WILL” CONTRACTIONS WITHOUT THE APOSTROPHE
+        HELLILLSHELLWELL
+ """,
+        """
+| AWESOME                           | DOPE, FIRE, LIT, SICK                          |
+| URL ENDINGS PLUS A LETTER         | COMP, MILK, NETI, ORGO                         |
+| DEFEAT SOUNDLY                    | CREAM, LICK, PASTE, SMOKE                      |
+| WILL CONTRACTION, SANS APOSTROPHE | HELL, ILL, SHELL, WELL                         |
+""",
+    ),
+    (
+        """
+Create four groups of four!
+
+4 FoundCategories out of 4
+
+    PLAY SOME ELECTRIC GUITAR
+        JAMNOODLESHREDSOLO
+    QUALITIES OF OVERCOOKED MEAT
+        CHEWYDRYSTRINGYTOUGH
+    INGREDIENTS IN BUBBLE TEA
+        BOBAMILKSUGARTEA
+    PLANETS/DWARF PLANET WITH FIRST LETTER CHANGED
+        BLUTOCARSDARTHGENUS
+""",
+        """.""",
+    ),
+]
