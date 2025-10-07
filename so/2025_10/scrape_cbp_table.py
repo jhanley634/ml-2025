26c6ff7b455449317c40157afffa75cfa49fa926
John Hanley <jhanley634@users.noreply.github.com>
2025-10-07 16:35:36 -0700

    With revised UA we now get "no tables found"

--- so/2025_10/scrape_cbp_table.py
+++ so/2025_10/scrape_cbp_table.py
@@ -2,7 +2,28 @@
 
 # from https://stackoverflow.com/questions/79784978/webpage-table-as-a-dataframe
 
+from io import StringIO
+
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+def get(url: str) -> StringIO:
+    headers = {"User-Agent": "Mozilla/5.0"}
+    resp = requests.get(url, headers=headers)
+    resp.raise_for_status()
+    return StringIO(resp.text)
+
+
+url = "https://www.cbp.gov/newsroom/stats/southwest-land-border-encounters"
+
+
+def main() -> None:
+    soup = BeautifulSoup(get(url), "lxml")
+    print(soup.prettify())
+    # pd.read_html(get(url))
+
 
 if __name__ == "__main__":
-    pd.read_html("https://www.cbp.gov/newsroom/stats/southwest-land-border-encounters")
+    main()
